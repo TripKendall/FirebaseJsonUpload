ee470d1165e03422180388ed8a2c68ca3fa7aa05
TripKendall <34070889+TripKendall@users.noreply.github.com>
2020-09-14 09:07:18 -0400

    Init Commit
    He adds the files to the repository...

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase Config
+const firebaseConfig = {
+    
+  // PUT YOUR FIREBASE CONFIG INFO HERE
+
+  // http://jtk.today/2020/08/16/importing-json-in-firebase-firestore/
+
+  }
+  // Exports
+  module.exports = firebaseConfig;
