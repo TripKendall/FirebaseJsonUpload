7c4b51d915aaa90648c5769339dcd8d846662d62
TripKendall <34070889+TripKendall@users.noreply.github.com>
2020-09-14 09:10:36 -0400

    Update import.js
    added a simple comment about default json file

--- import.js
+++ import.js
@@ -8,10 +8,11 @@ const jsonToFirestore = async () => {
   try {
     console.log('Initialzing Firebase');
     await firestoreService.initializeApp(serviceAccount, firebaseConfig.databaseURL);
-    console.log('Firebase Initialized');
-
+    console.log('The Database is ready');
+    
+    // this is just the included json file - replace with yours to import your data
     await firestoreService.restore('./btc-twitter.json');
-    console.log('Upload Success');
+    console.log('Your Json Uploaded! Success!');
   }
   catch (error) {
     console.log(error);
