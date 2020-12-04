497d5669a7a708781458b586defe6105ff7fd591
TripKendall <34070889+TripKendall@users.noreply.github.com>
2020-12-04 11:19:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,6 @@ const firebaseConfig = {
     
   // PUT YOUR FIREBASE CONFIG INFO HERE
 
-  // http://jtk.today/2020/08/16/importing-json-in-firebase-firestore/
-
   }
   // Exports
   module.exports = firebaseConfig;
